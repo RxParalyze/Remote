ef743b50ed3453f6f314068eb8e78ca5781c28ac
RxParalyze <c17phillip.smart@gmail.com>
2020-06-27 14:48:12 -0400

    removed external libraries

---
+++ src/runController.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#wired: 68:27:37:2F:A5:3A
+#wireless: E4:7D:BD:BF:E5:0D
+#bluetooth: E4:7D:BD:BF:E5:0E
+#smart control bluetooth: D8:E0:E1:51:F3:F1
+#unique id: GU556Y66I72HH
+#unique device id: 43CFUGIO5N2PQ
+
+config = {
+    "name" : 'samsungctl',
+    "id" : 'Smart Device',
+    "port" : 55000,
+    "mac" : 'E4:7D:BD:BF:E5:0D',
+    "timeout" : 10,
+    "host" : '192.168.1.42'
+}
--- src/test.py
+++ src/test.py
@@ -3,12 +3,13 @@ import samsungctl
 config = {
     "name": "samsungctl",
     "description": "PC",
-    "id": "D8:E0:E1:51:F3:F1",
+    "id": "68:27:37:2F:A5:3A",
     "host": "192.168.1.42",
     "port": 1515,
-    "method": "legacy",
+    "method": "websocket",
     "timeout": 0,
 }
 
 with samsungctl.Remote(config) as remote:
-    remote.control("KEY_POWEROFF")
+    remote.connection.connect()
+    remote.control("KEY_POWER")
